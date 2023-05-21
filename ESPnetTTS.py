b69bd6a783fc9cbc87447393014b0c2630ddaa24
Kevin Harrington <mad.hephaestus@gmail.com>
2023-05-21 14:50:34 -0400

    adding code

--- ESPnetTTS.py
+++ ESPnetTTS.py
@@ -1,1 +1,47 @@
-print( 'Hello World')
+import time
+import torch
+
+from espnet2.bin.tts_inference import Text2Speech
+from espnet2.utils.types import str_or_none
+
+#@title Choose English model { run: "auto" }
+lang = 'English'
+tag = 'kan-bayashi/ljspeech_vits' #@param ["kan-bayashi/ljspeech_tacotron2", "kan-bayashi/ljspeech_fastspeech", "kan-bayashi/ljspeech_fastspeech2", "kan-bayashi/ljspeech_conformer_fastspeech2", "kan-bayashi/ljspeech_joint_finetune_conformer_fastspeech2_hifigan", "kan-bayashi/ljspeech_joint_train_conformer_fastspeech2_hifigan", "kan-bayashi/ljspeech_vits"] {type:"string"}
+vocoder_tag = "none" #@param ["none", "parallel_wavegan/ljspeech_parallel_wavegan.v1", "parallel_wavegan/ljspeech_full_band_melgan.v2", "parallel_wavegan/ljspeech_multi_band_melgan.v2", "parallel_wavegan/ljspeech_hifigan.v1", "parallel_wavegan/ljspeech_style_melgan.v1"] {type:"string"}
+
+
+
+text2speech = Text2Speech.from_pretrained(
+    model_tag=str_or_none(tag),
+    vocoder_tag=str_or_none(vocoder_tag),
+    device="cuda",
+    # Only for Tacotron 2 & Transformer
+    threshold=0.5,
+    # Only for Tacotron 2
+    minlenratio=0.0,
+    maxlenratio=10.0,
+    use_att_constraint=False,
+    backward_window=1,
+    forward_window=3,
+    # Only for FastSpeech & FastSpeech2 & VITS
+    speed_control_alpha=1.0,
+    # Only for VITS
+    noise_scale=0.333,
+    noise_scale_dur=0.333,
+)
+
+
+# decide the input sentence by yourself
+print(f"Input your favorite sentence in {lang}.")
+x = input()
+
+# synthesis
+with torch.no_grad():
+    start = time.time()
+    wav = text2speech(x)["wav"]
+rtf = (time.time() - start) / (len(wav) / text2speech.fs)
+print(f"RTF = {rtf:5f}")
+
+# let us listen to generated samples
+from IPython.display import display, Audio
+display(Audio(wav.view(-1).cpu().numpy(), rate=text2speech.fs))
